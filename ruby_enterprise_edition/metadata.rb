9bec4dc733b501c4112877e6ca38e9821fdca92a
Joshua Sierles <joshua@diluvia.net>
2011-07-07 12:38:23 +0200

    add REE cookbook

---
+++ ruby_enterprise_edition/metadata.rb
@@ -0,0 +1,5 @@
+maintainer        "37signals"
+maintainer_email  "sysadmins@37signals.com"
+description       "Configures REE"
+version           "0.1"
+depends           "ruby"
---
+++ ruby_enterprise_edition/recipes/default.rb
@@ -0,0 +1,20 @@
+remote_file "/tmp/#{node[:ree][:filename]}" do
+  source "#{node[:ree][:base_url]}/#{node[:ree][:filename]}"
+end
+
+dpkg_package "ruby-enterprise" do
+  source "/tmp/#{node[:ree][:filename]}"
+end
+
+gem_package "rake" do
+  options :force => true
+  not_if "gem list | grep rake"
+end
+
+gem_package "rack" do
+  options :force => true
+  not_if "gem list | grep rack"
+end
+
+include_recipe "ruby::gc_wrapper"
+
