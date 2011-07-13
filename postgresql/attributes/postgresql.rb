f62be3ece480bfb96b194d65e9655f1dec74ec37
Joshua Sierles <joshua@diluvia.net>
2011-07-13 21:53:47 +0200

    listen on local networks

--- postgresql/attributes/postgresql.rb
+++ postgresql/attributes/postgresql.rb
@@ -1,2 +1,5 @@
 default[:postgresql][:version] = "9.0"
 default[:postgresql][:dir] = "/etc/postgresql/#{node[:postgresql][:version]}/main"
+
+# comma-separated, * means all
+default[:postgresql][:listen_addresses] = "*"
