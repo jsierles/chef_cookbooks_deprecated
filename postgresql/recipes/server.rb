19545f93b99c366e4babc44f49ee6fff3953ee82
Joshua Sierles <joshua@diluvia.net>
2011-07-15 19:02:44 +0200

    support recovery and hot standby mode

--- postgresql/recipes/server.rb
+++ postgresql/recipes/server.rb
@@ -1,5 +1,6 @@
 package "postgresql-#{node[:postgresql][:version]}"
 package "libpq-dev"
+package "postgresql-server-dev-#{node[:postgresql][:version]}"
 
 include_recipe "postgresql::client"
 
@@ -13,7 +14,7 @@ template "#{node[:postgresql][:dir]}/pg_hba.conf" do
   source "pg_hba.conf.erb"
   owner "postgres"
   group "postgres"
-  mode 0600
+  mode 0644
   notifies :reload, resources(:service => "postgresql")
 end
 
@@ -21,6 +22,24 @@ template "#{node[:postgresql][:dir]}/postgresql.conf" do
   source "postgresql.conf.erb"
   owner "postgres"
   group "postgres"
-  mode 0600
-  notifies :restart, resources(:service => "postgresql")
+  mode 0644
+  
+  # disabled to prevent accidental restarts in production
+  #notifies :restart, resources(:service => "postgresql")
+end
+
+if node[:postgresql][:role] == "slave"
+  template "#{node[:postgresql][:dir]}/recovery.conf" do
+    source "recovery.conf.erb"
+    owner "postgres"
+    group "postgres"
+    mode 0644
+    
+    # disabled to prevent accidental restarts in production    
+    #notifies :restart, resources(:service => "postgresql")
+  end  
+else
+  file "#{node[:postgresql][:dir]}/recovery.conf" do
+    action :delete
+  end
 end
