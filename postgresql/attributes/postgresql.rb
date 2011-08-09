b0706df31accd68c9326c5624e25bca04cc89379
Joshua Sierles <joshua@diluvia.net>
2011-08-09 20:37:11 +0200

    updates for streaming replication

--- postgresql/attributes/postgresql.rb
+++ postgresql/attributes/postgresql.rb
@@ -1,5 +1,6 @@
 default[:postgresql][:version] = "9.0"
-default[:postgresql][:dir] = "/etc/postgresql/#{node[:postgresql][:version]}/main"
+default[:postgresql][:config_dir] = "/etc/postgresql/#{node[:postgresql][:version]}/main"
+default[:postgresql][:datadir] = "/var/lib/postgresql/#{node[:postgresql][:version]}/main"
 
 # comma-separated, * means all
 default[:postgresql][:listen_addresses] = "*"
--- postgresql/recipes/server.rb
+++ postgresql/recipes/server.rb
@@ -4,13 +4,19 @@ package "postgresql-server-dev-#{node[:postgresql][:version]}"
 
 include_recipe "postgresql::client"
 
+directory "/etc/postgresql" do
+  mode 0755
+  owner "postgres"
+  group "postgres"
+end
+
 service "postgresql" do
   service_name "postgresql"
   supports :restart => true, :status => true, :reload => true
   action :nothing
 end
 
-template "#{node[:postgresql][:dir]}/pg_hba.conf" do
+template "#{node[:postgresql][:config_dir]}/pg_hba.conf" do
   source "pg_hba.conf.erb"
   owner "postgres"
   group "postgres"
@@ -18,7 +24,7 @@ template "#{node[:postgresql][:dir]}/pg_hba.conf" do
   notifies :reload, resources(:service => "postgresql")
 end
 
-template "#{node[:postgresql][:dir]}/postgresql.conf" do
+template "#{node[:postgresql][:config_dir]}/postgresql.conf" do
   source "postgresql.conf.erb"
   owner "postgres"
   group "postgres"
@@ -29,7 +35,7 @@ template "#{node[:postgresql][:dir]}/postgresql.conf" do
 end
 
 if node[:postgresql][:role] == "slave"
-  template "#{node[:postgresql][:dir]}/recovery.conf" do
+  template "#{node[:postgresql][:datadir]}/recovery.conf" do
     source "recovery.conf.erb"
     owner "postgres"
     group "postgres"
@@ -39,7 +45,7 @@ if node[:postgresql][:role] == "slave"
     # notifies :restart, resources(:service => "postgresql")
   end  
 else
-  file "#{node[:postgresql][:dir]}/recovery.conf" do
+  file "#{node[:postgresql][:datadir]}/recovery.conf" do
     action :delete
   end
 end
