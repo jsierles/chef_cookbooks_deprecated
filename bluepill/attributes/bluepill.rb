99d399f57f2dfe7b6774654eace3e17a0cf8d5a7
Joshua Sierles <joshua@diluvia.net>
2012-03-06 19:44:49 -0300

    update bluepill version

--- bluepill/attributes/bluepill.rb
+++ bluepill/attributes/bluepill.rb
@@ -7,4 +7,4 @@ default.bluepill[:log_dir] = "/var/log/bluepill"
 default.bluepill[:pid_dir] = "/var/run/bluepill"
 default[:bluepill][:state_dir] = "/var/lib/bluepill"
 
-bluepill[:version] = "0.0.51"
+bluepill[:version] = "0.0.60"
