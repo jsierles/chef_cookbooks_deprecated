890f6740510fe78113367518d3b681dbbcd8a3d0
Joshua Sierles <joshua@diluvia.net>
2012-05-01 13:04:43 -0400

    update ruby-shadow for 1.9

--- ruby-shadow/recipes/default.rb
+++ ruby-shadow/recipes/default.rb
@@ -1,15 +1,15 @@
-remote_directory "/usr/local/src/shadow-1.4.1" do
-  source 'shadow-1.4.1'
-  not_if { File.exists?("/usr/local/lib/ruby/site_ruby/1.8/#{node[:languages][:ruby][:platform]}/shadow.so") }
+remote_directory "/usr/local/src/shadow-2.1.3" do
+  source 'shadow-2.1.3'
+  not_if { File.exists?("#{node[:languages][:ruby][:bin_dir].gsub(/bin$/, "lib/ruby/site_ruby/1.9.1/")}#{node[:languages][:ruby][:platform]}/shadow.so") }
 end
 
 bash "install ruby shadow library" do
   user "root"
   cwd "/usr/local/src"
   code <<-EOH
-  cd shadow-1.4.1
+  cd shadow-2.1.3
   ruby extconf.rb
   make install
   EOH
-  not_if { File.exists?("/usr/local/lib/ruby/site_ruby/1.8/#{node[:languages][:ruby][:platform]}/shadow.so") }
+  not_if { File.exists?("#{node[:languages][:ruby][:bin_dir].gsub(/bin$/, "lib/ruby/site_ruby/1.9.1/")}#{node[:languages][:ruby][:platform]}/shadow.so") }
 end
