913512f65b3c7082feb3661cb5e04ed93001054b
Joshua Sierles <joshua@diluvia.net>
2012-09-18 19:27:10 +0200

    add gravatar recipe

---
+++ nginx/recipes/gravatar.rb
@@ -0,0 +1,13 @@
+require_recipe "nginx"
+
+directory "/var/cache/nginx/proxy-cache" do
+  owner "www-data"
+end
+
+template "/etc/nginx/sites-available/gravatar.conf" do
+  notifies :reload, resources(:service => "nginx")
+end
+
+nginx_site 'gravatar' do
+  action :enable
+end
